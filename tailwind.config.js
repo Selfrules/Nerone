607596a5700b70d6aa853d55972aec6d6f089d49
Selfrules <mattia@selfrules.org>
2022-04-16 18:51:29 +0200

    fix tailwind, add elements, add mana cap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    color:{
+      primary: colors.cyan,
+      secondary: colors.slate,
+      neutral: colors.gray,
+    },
   },
   plugins: [],
 }
